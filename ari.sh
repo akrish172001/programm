b0521477a7bb4a5bcfb7705831134aa1316ad252
akrish172001 <adarshunnikrishnancr7@gmail.com>
2023-01-13 19:21:56 +0530

    third commit

---
+++ ari.sh
@@ -0,0 +1,12 @@
+echo " enter 2 no "
+read a
+read b
+s=`expr $a + $b `
+echo " sum is $s "
+m=`expr $a - $b `
+echo " difference is $m "
+p=`expr $a \* $b `
+echo " product is $p "
+d=`expr $a / $b `
+echo " divisor is $d "
+
